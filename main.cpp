88ad9ef06da8b0e02c0e5638bedf16c7c81441aa
Deyan Dobromirov <dvd_video@abv.bg>
2016-06-30 15:07:37 +0300

    Added: Two timer modes

--- main.cpp
+++ main.cpp
@@ -1,47 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
-#include <time.h>
+#include <windows.h>
 #include "time_interval.h"
 
 tmi D, T;
+int pitch     =  0 ;
+char key      =  0 ;
+char buf[256] = {0};
+
+char getKeyboard(void)
+{
+  if(kbhit()){ return getch(); } return 0x20;
+}
 
 void isrTimer(void)
 {
-  printf("[%5d]This ia a test\n",T.getTickRate());
+  if(!pitch){
+    printf(" Using: <%s>",buf);
+  }else{
+    printf(" Using pitch of: <%d>",pitch);
+  }
 }
 
-int main()
+int main(int argc, char** argv)
 {
-  char key;
-  unsigned int cy = 50;
-  D.Set(5,0,0);
+  TIME_INTERVAL argi[3] = {0};
+  if(argc > 1){
+    printf("\nargc = %d",argc);
+    for(int i = 1; ((i < 4) && (i < argc)); i++)
+    {
+      argi[i-1] = (TIME_INTERVAL)atoi(argv[i]);
+      printf("\nargv[%d] = <%s> #%u",i,argv[i],argi[i-1]);
+    }
+  }
+
+  printf("\nAlarm type: "); gets(buf);
+
+  pitch = atoi(buf);
+  if(pitch){
+    sscanf(buf,"%d %u %u %u",&pitch,&argi[0],&argi[1],&argi[2]);
+  }
+
+  D.Set(argi[0],argi[1],argi[2]);
 
   T.Set(D);
   T.setHook(isrTimer);
-  T.setTickRate(cy);
 
-  while(42)
+  while(27 != (key = getKeyboard()))
   {
-    if ( kbhit() ){ key = getch(); }
-    switch(key)
-    {
-      case 'l':
-      {
-        cy++;
-        T.setTickRate(cy); break;
-      }
-      case 'k':
-      {
-        cy--;
-        T.setTickRate(cy); break;
-      }
-    }
-    T.Normalize().Decrement().Tick();
-    if(T.isPassed())
-    {
-      T.callHook().Set(D);
-    }
-    key = 'x';
+    T.Normalize().Tick().Decrement();
+    if(T.isPassed()){ T.callHook().Set(D); }
+    T.Print(key);
   }
-  return 0;
+  return EXIT_SUCCESS;
 }
--- time_interval.h
+++ time_interval.h
@@ -1,62 +1,60 @@
 #if !defined _TIME_INTERVAL_H_
-
+  #include <time.h>
   #define _TIME_INTERVAL_H_
-
+  #define TIME_INTERVAL unsigned int
   typedef class time_interval
   {
     protected:
-       int clk;
-       int h;
-       int m;
-       int s;
-      void (*hook)(void);
+       TIME_INTERVAL clk;
+       TIME_INTERVAL h;
+       TIME_INTERVAL m;
+       TIME_INTERVAL s;
+       void (*hook)(void);
     public:
-       char isPassed(void){ return (s==0 && m==0 && h==0); }
-      class time_interval& Set(class time_interval &o){ s = o.s; m = o.m; h = o.h; return *this; }
-      class time_interval& Set(class time_interval *o){ if(o != NULL){ s = o->s; m = o->m; h = o->h; } return *this; }
-      class time_interval& Set(int as, int am, int ah){ s = as; m = am; h = ah; return *this; }
-            time_interval(int as, int am, int ah){ Set(as,am,ah); clk = CLOCKS_PER_SEC; }
-            time_interval(int as, int am){ Set(as,am,0); clk = CLOCKS_PER_SEC; }
-            time_interval(int as){ Set(as,0,0); clk = CLOCKS_PER_SEC; }
-            time_interval(void){ Set(0,0,0); clk = CLOCKS_PER_SEC; }
-           ~time_interval(){}
+       char isPassed(void){ return (s==0 && m==0 && h==0); };
+      class time_interval& Set(class time_interval &o){ s = o.s; m = o.m; h = o.h; return *this; };
+      class time_interval& Set(class time_interval *o){ if(o != NULL){ s = o->s; m = o->m; h = o->h; } return *this; };
+      class time_interval& Set(TIME_INTERVAL as, TIME_INTERVAL am, TIME_INTERVAL ah){ s = as; m = am; h = ah; return *this; };
+            time_interval(TIME_INTERVAL as, TIME_INTERVAL am, TIME_INTERVAL ah){ Set(as,am,ah); clk = CLOCKS_PER_SEC; };
+            time_interval(TIME_INTERVAL as, TIME_INTERVAL am){ Set(as,am,0); clk = CLOCKS_PER_SEC; };
+            time_interval(TIME_INTERVAL as){ Set(as,0,0); clk = CLOCKS_PER_SEC; };
+            time_interval(void){ Set(0,0,0); clk = CLOCKS_PER_SEC; };
+           ~time_interval(){};
+      class time_interval& Wait(clock_t next){ while(clock() < next); return *this; };
       class time_interval& Reset(){ Set(0,0,0); return *this; }
-      class time_interval& setS(int as){ s = as; return *this; }
-      class time_interval& setM(int am){ m = am; return *this; }
-      class time_interval& setH(int ah){ h = ah; return *this; }
-              unsigned int getS(void){ return s; }
-              unsigned int getM(void){ return m; }
-              unsigned int getH(void){ return h; }
-      class time_interval& setHook(void (*foo)(void)){ hook = foo; return *this; }
-      class time_interval& callHook(void){ (*hook)(); return *this; };
-      class time_interval& setTickRate(int cy){ if(cy > 0){ clk = cy; } return *this; };
-              unsigned int getTickRate(void){ return clk; };
+      class time_interval& setS(TIME_INTERVAL as){ s = as; return *this; }
+      class time_interval& setM(TIME_INTERVAL am){ m = am; return *this; }
+      class time_interval& setH(TIME_INTERVAL ah){ h = ah; return *this; }
+            TIME_INTERVAL  getS(void){ return s; }
+            TIME_INTERVAL  getM(void){ return m; }
+            TIME_INTERVAL  getH(void){ return h; }
+      class time_interval& setHook(void (*foo)(void)){ hook = foo;   return *this; }
+      class time_interval& callHook(void){ if((*hook)){ (*hook)(); } return *this; };
+      class time_interval& setTickRate(TIME_INTERVAL cy){ clk = cy;  return *this; };
+      class time_interval& deltaTickRate(int dcy);
+            TIME_INTERVAL  getTickRate(void){ return clk; };
       class time_interval& Normalize(void);
       class time_interval& Decrement(void){ if(s > 0){ s--; } return *this; }
-      class time_interval& Print(void){ printf("Diff(%d): %15d, %15d, %15d\n",clk,s,m,h); return *this; };
-      class time_interval& TickSecond(void);
-      class time_interval& Tick(void);
+      class time_interval& Print(void){ printf("\nTime(%u): %5u, %5u, %5u",clk,s,m,h); return *this; };
+      class time_interval& Print(char c){ printf("\nTime(%u)[%c]: %5u, %5u, %5u",clk,c,s,m,h); return *this; };
+      class time_interval& Print(char *str){ printf("\nTime(%u)[%s]: %5u, %5u, %5u",clk,str,s,m,h); return *this; };
+      class time_interval& TickSecond(void){ return Wait(clock() + CLOCKS_PER_SEC); }
+      class time_interval& TickSecond(TIME_INTERVAL s){ return Wait(clock() + (s * CLOCKS_PER_SEC)); }
+      class time_interval& Tick(void){ return Wait(clock() + clk); }
   } tmi;
 
   tmi& time_interval::Normalize(void)
   {
-    if(!m && h > 0){ m = 60; h--; }
-    if(!s && m > 0){ s = 60; m--; }
+    if(!m && h){ m = 60; h--; }
+    if(!s && m){ s = 60; m--; }
     return *this;
   }
 
-  tmi& time_interval::Tick()
+  tmi& time_interval::deltaTickRate(int dcy)
   {
-    clock_t next = clock() + clk;
-    while(clock() < next);
-    return *this;
-  }
-
-  tmi& time_interval::TickSecond(void)
-  {
-    clock_t next = clock() + CLOCKS_PER_SEC;
-    while(clock() < next);
-    return *this;
+    if((dcy > 0) && (clk + dcy) < clk){ return *this; }
+    if((dcy < 0) && (clk + dcy) > clk){ return *this; }
+    return setTickRate(clk + dcy);
   }
 
 #endif
